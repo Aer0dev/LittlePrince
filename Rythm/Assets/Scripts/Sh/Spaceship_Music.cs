d7bde6534eb6707fa903bef4e99c6e1c9eed1456
Aer0dev <45550827+Aer0dev@users.noreply.github.com>
2023-05-22 23:57:24 +0900

    Revert "0522"
    This reverts commit 841a9db9b76215f81fdcaa33adad80cb5206a149.

--- Rythm/Assets/Scripts/Sh/Spaceship_Music.cs
+++ Rythm/Assets/Scripts/Sh/Spaceship_Music.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class Spaceship_Music : MonoBehaviour
 {
-    private AudioSource audioSource;
+    public AudioSource audioSoruce;
 
     private double startTime;
 
     public double delayInSeconds = 3.0;     // 음악 3초 뒤에 시작하도록
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.mute = false;
+        audioSoruce = GetComponent<AudioSource>();
         startTime = AudioSettings.dspTime;
-        audioSource.PlayScheduled(startTime + delayInSeconds);
+        audioSoruce.PlayScheduled(startTime + delayInSeconds);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
