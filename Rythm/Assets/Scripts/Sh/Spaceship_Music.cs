841a9db9b76215f81fdcaa33adad80cb5206a149
Aer0dev <45550827+Aer0dev@users.noreply.github.com>
2023-05-22 23:54:22 +0900

    0522

--- Rythm/Assets/Scripts/Sh/Spaceship_Music.cs
+++ Rythm/Assets/Scripts/Sh/Spaceship_Music.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 
 public class Spaceship_Music : MonoBehaviour
 {
-    public AudioSource audioSoruce;
+    private AudioSource audioSource;
 
     private double startTime;
 
     public double delayInSeconds = 3.0;     // 음악 3초 뒤에 시작하도록
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        audioSoruce = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.mute = false;
         startTime = AudioSettings.dspTime;
-        audioSoruce.PlayScheduled(startTime + delayInSeconds);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        audioSource.PlayScheduled(startTime + delayInSeconds);
     }
 }
