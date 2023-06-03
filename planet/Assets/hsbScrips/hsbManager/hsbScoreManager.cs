04363ca673a419676639d3c8cbba366a43334243
[hsb031106@naver.com] <hsb031106@naver.com>
2023-06-03 23:49:54 +0900

    Final

--- planet/Assets/hsbScrips/hsbManager/hsbScoreManager.cs
+++ planet/Assets/hsbScrips/hsbManager/hsbScoreManager.cs
@@ -7,15 +7,23 @@ public class hsbScoreManager : MonoBehaviour
 {
     public float score;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameClear;
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
+        gameClear.gameObject.SetActive(false);
+    }
+
+    void startGame()
+    {
+        gameClear.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         scoreText.text = Mathf.FloorToInt(score).ToString();
+
     }
 }
